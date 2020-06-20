b7546b3b6c168890027663e9daf9348b80e53e67
Abhay <chauhan27.abhay@gmail.com>
2020-06-20 07:16:38 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let quotes = [
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "First, solve the problem. Then, write the code.",
+    "Experience is the name everyone gives to their mistakes.",
+    "In order to be irreplaceable, one must always be different",
+    "Java is to JavaScript what car is to Carpet.",
+    "Knowledge is power."
+];
+
+function quoteGenerate(){
+    var quote = Math.floor(Math.random()* quotes.length);
+
+    document.getElementById("parah").innerHTML = quotes[quote];
+}
+
